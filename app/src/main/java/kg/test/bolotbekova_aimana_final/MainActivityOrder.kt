fc152f13aa23e1cb88ebcd797defd639d4abcbae
aimanabolotbekova <bolotbekova_a@auca.kg>
2021-12-13 14:41:44 +0600

    commit

---
+++ app/src/main/java/kg/test/bolotbekova_aimana_final/MainActivityOrder.kt
@@ -0,0 +1,11 @@
+package kg.test.bolotbekova_aimana_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order )
+    }
+}
